df24294527d930246b5c36cf2dfd492da97fb7ca
Atharva <deshmukhsambhajirao9@gmail.com>
2024-09-28 12:48:42 +0530

    added some changes for styling, loaded env for running locally

--- src/pages/CrossChain.jsx
+++ src/pages/CrossChain.jsx
@@ -1,5 +1,5 @@
 import WormholeConnect from "@wormhole-foundation/wormhole-connect";
-import "./cross.css";
+// import "./cross.css";
 const config = {
   showHamburgerMenu: false,
   env: "mainnet"
@@ -7,8 +7,8 @@ const config = {
 
 function CrossChain() {
   return (
-    <div className=" bg-gradient flex flex-col gap-5 min-w-full justify-center items-center ">
-      <h1 className="mt-10 text-6xl font-bold text-black tracking-wide">
+    <div className=" bg-gradient flex flex-col gap-5 w-screen justify-center items-center bg-black">
+      <h1 className="mt-10 text-6xl font-bold text-white tracking-wide">
         CrossKit
       </h1>
       <p className="font-sans text-2xl text-slate-400">Transfer from one chain to another in few clicks</p>
